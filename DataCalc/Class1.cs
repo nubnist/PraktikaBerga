28b49d46446375d14aa6dfaa92a4d806a0353281
nubnist <44030910+nubnist@users.noreply.github.com>
2021-02-04 22:22:03 +0300

    Были исключены лишние преобразования между радианами и градусами.

--- DataCalc/Class1.cs
+++ DataCalc/Class1.cs
@@ -158,14 +158,14 @@ namespace DataCalc
             var r2 = e2 * (R + param.h);
             
             // Угловая величина дуги большого круга, соединяющей начальную и конечную точки трассы.
-            var delta = TD(Math.Acos(e1 * e2));
+            var delta = Arccos(e1 * e2);
             
             // Угловая величина дуги большого круга, пройденной за время t.
             var alpha = ((param.V * param.t) / (R + param.h)) * (180 / Math.PI);
 
-            #region Используется для расчетов в следующих строках для расчета векторов.
-            Func<GeocentrCoord, GeocentrCoord> npr = obj => obj * (Math.Sin(delta - alpha) / Math.Sin(alpha)) + 
-                                                            obj * (Math.Sin(alpha) / Math.Sin(delta));
+            #region Используется в следующих строках для расчета векторов.
+            Func<GeocentrCoord, GeocentrCoord> npr = obj => obj * (Sin(delta - alpha) / Sin(alpha)) + 
+                                                            obj * (Sin(alpha) / Sin(delta));
             #endregion
             
             // Вектор координат ЛА в момент t.
@@ -176,15 +176,15 @@ namespace DataCalc
             
             // Географические координаты ЛА в момент t.
             var f = Math.Asin(e.Z);
-            var lambda = TD(Math.Acos(e.X / TD(Math.Cos(f))));
-            f = TD(f);
+            var lambda = Arccos(e.X / Cos(f));
+            f = ToDegrees(f);
             if (e.Y < 0) lambda *= -1;
             else if (e.Y == 0) lambda *= 0;
             
             //Вектор скорости ЛА в момент t
             var p_speed = 
-                (r2 * (TD(Math.Cos(TR(alpha))) / TD(Math.Sin(TR(delta))))
-                 - r1 * (Math.Cos(TR(delta - alpha)) / Math.Sin(TR(delta)))) 
+                (r2 * (Cos(alpha) / Sin(delta))
+                 - r1 * (Cos(delta - alpha) / Sin(delta)))
                 * (param.V / (R + param.h));
 
             return new TrassalOutParam();
@@ -200,17 +200,50 @@ namespace DataCalc
         private static GeocentrCoord ToGeocetnricCoord(GeographCoord coord) =>
             new GeocentrCoord()
             {
-                X = Math.Cos(TR(coord.Fi)) * Math.Cos(TR(coord.Lambda)),
-                Y = Math.Cos(TR(coord.Fi)) * Math.Sin(TR(coord.Lambda)),
-                Z = Math.Sin(TR(coord.Fi))
+                X = Math.Cos(ToRadian(coord.Fi)) * Math.Cos(ToRadian(coord.Lambda)),
+                Y = Math.Cos(ToRadian(coord.Fi)) * Math.Sin(ToRadian(coord.Lambda)),
+                Z = Math.Sin(ToRadian(coord.Fi))
             };
+
+        /// <summary>
+        /// Возвращает sin
+        /// </summary>
+        /// <param name="degr">Градусы</param>
+        /// <returns>Sin</returns>
+        private static double Sin(double degr) =>
+            Math.Sin(ToRadian(degr));
+        
+        /// <summary>
+        /// Возвращает сos
+        /// </summary>
+        /// <param name="degr">Градусы</param>
+        /// <returns>Cos в грудсах</returns>
+        private static double Cos(double degr) =>
+            Math.Cos(ToRadian(degr));
+        
+        /// <summary>
+        /// Возвращает Arcsin
+        /// </summary>
+        /// <param name="nm">Число</param>
+        /// <returns>Arcsin</returns>
+        private double Arcsin(double nm) =>
+            ToDegrees(Math.Asin(nm));
+        
+        /// <summary>
+        /// Возвращает Arccos
+        /// </summary>
+        /// <param name="nm">Число</param>
+        /// <returns>Arccos</returns>
+        private static double Arccos(double nm) =>
+            ToDegrees(Math.Acos(nm));
+        
 
         /// <summary>
         /// Переводит градусы в радианы
         /// </summary>
         /// <param name="degrees">Градусы</param>
         /// <returns>Радианы</returns>
-        private static double TR(double degrees) =>
+        private static double ToRadian(double degrees) =>
             (degrees * Math.PI) / 180;
 
         
@@ -219,7 +252,7 @@ namespace DataCalc
         /// </summary>
         /// <param name="rad">Радианы</param>
         /// <returns>Градусы</returns>
-        private static double TD(double rad) =>
+        private static double ToDegrees(double rad) =>
             (rad * 180) / Math.PI;
 
         #endregion
